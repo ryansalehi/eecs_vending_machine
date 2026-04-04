8f0d368d27f486e15665137820fc367e542f2e7b
Salehi <salehir@UMICH.EDU>
2026-04-04 17:18:31 -0400

    KEYPAD: Decode readings

--- MCU_A_v1_3/Core/Inc/keypad.h
+++ MCU_A_v1_3/Core/Inc/keypad.h
@@ -1,4 +1,14 @@
+/**
+ * @file keypad.h
+ * 
+ * @date April 4, 2026
+ */
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
 
 void KEYPAD_IrqFromIsr();
 void KEYPAD_ReadAnyPresses();
 void KEYPAD_Init();
+
+#endif //KEYPAD_H_

36bbf1e441db588374968f02587594af08cf7c57
Ryan Salehi <ryanjsalehi@gmail.com>
2026-04-07 14:46:44 -0400

    V2: Reinit MCU_B

---
+++ MCU_B_v2_0/Core/Src/main.c
@@ -0,0 +1,498 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+#include "mbedtls.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "nfc.h"
+#include "ps2.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+/* Definitions for NFCTask */
+osThreadId_t NFCTaskHandle;
+const osThreadAttr_t NFCTask_attributes = {
+  .name = "NFCTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for PS2Task */
+osThreadId_t PS2TaskHandle;
+const osThreadAttr_t PS2Task_attributes = {
+  .name = "PS2Task",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for UARTTask */
+osThreadId_t UARTTaskHandle;
+const osThreadAttr_t UARTTask_attributes = {
+  .name = "UARTTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* USER CODE BEGIN PV */
+NFC_handle nfc =
+{
+		.hi2c = &hi2c1,
+		.i2c_addr = PN532_I2C_ADDR_8BIT,
+    .irq_port = GPIOC, .irq_pin = GPIO_PIN_13,
+    .rst_port = GPIOA, .rst_pin = GPIO_PIN_10
+};
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+void StartNFCTask(void *argument);
+void StartPS2Task(void *argument);
+void StartUARTTask(void *argument);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  /* Call PreOsInit function */
+  MX_MBEDTLS_Init();
+  /* USER CODE BEGIN 2 */
+  if(HAL_OK != NFC_Init(&nfc))
+  {
+    while(true)
+    {
+      HAL_Delay(1000);
+    }
+  }
+
+  PS2_Init();
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of NFCTask */
+  NFCTaskHandle = osThreadNew(StartNFCTask, NULL, &NFCTask_attributes);
+
+  /* creation of PS2Task */
+  PS2TaskHandle = osThreadNew(StartPS2Task, NULL, &PS2Task_attributes);
+
+  /* creation of UARTTask */
+  UARTTaskHandle = osThreadNew(StartUARTTask, NULL, &UARTTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 360;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Activate the Over-Drive mode
+  */
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LD2_Pin|GPIO_PIN_8, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PC0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PC1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC2 PC3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : USART_TX_Pin USART_RX_Pin */
+  GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LD2_Pin PA8 */
+  GPIO_InitStruct.Pin = LD2_Pin|GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC10 PC11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF8_UART4;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  if(GPIO_Pin == GPIO_PIN_13)
+  {
+    NFC_IrqFromIsr(&nfc);
+  }
+
+  if(GPIO_Pin == PS_2_Clock_Pin)
+  {
+	  PS2_ClockEdgeFromIsr();
+  }
+}
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartNFCTask */
+/**
+  * @brief  Function implementing the NFCTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartNFCTask */
+void StartNFCTask(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* USER CODE BEGIN NFCTask */
+  /* Infinite loop */
+  uint8_t last_uid[10] = {0};
+    uint8_t last_len = 0;
+
+    for (;;) {
+
+      if(HAL_OK == NFC_ReadPassiveTargetID(&nfc, last_uid, &last_len, 1000))
+      {
+        if(last_uid[0] == 0x9a)
+        {
+          HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+        }
+        osDelay(3000); // found a tag, wait a little bit to rescan
+      }
+      osDelay(1);
+    }
+  /* USER CODE END NFCTask */
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartPS2Task */
+/**
+* @brief Function implementing the PS2Task thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartPS2Task */
+void StartPS2Task(void *argument)
+{
+  /* USER CODE BEGIN StartPS2Task */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartPS2Task */
+}
+
+/* USER CODE BEGIN Header_StartUARTTask */
+/**
+* @brief Function implementing the UARTTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartUARTTask */
+void StartUARTTask(void *argument)
+{
+  /* USER CODE BEGIN StartUARTTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartUARTTask */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6)
+  {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ MCU_B_v2_0/Core/Src/nfc.c
@@ -0,0 +1,295 @@
+#include "nfc.h"
+#include <string.h>
+#include "cbuf.h"
+
+/* PN532 framing constants */
+#define PN532_PREAMBLE     0x00
+#define PN532_STARTCODE1   0x00
+#define PN532_STARTCODE2   0xFF
+#define PN532_POSTAMBLE    0x00
+
+#define PN532_HOSTTOPN532  0xD4
+#define PN532_PN532TOHOST  0xD5
+
+#define PN532_CMD_SAMCONFIGURATION     0x14
+#define PN532_CMD_INLISTPASSIVETARGET  0x4A
+
+/* Typical PN532 ready latency */
+#define PN532_ACK_TIMEOUT_TICKS   pdMS_TO_TICKS(200)
+#define PN532_RSP_TIMEOUT_TICKS   pdMS_TO_TICKS(500)
+
+/* Example: IRQ on PA10, RST on PA8 (change to your actual pins) */
+#define PN532_IRQ_PORT GPIOA
+#define PN532_IRQ_PIN  GPIO_PIN_10
+
+#define PN532_RST_PORT GPIOA
+#define PN532_RST_PIN  GPIO_PIN_8
+
+NFC_uid uidsArray[10];
+cbuf_t uids;
+static bool IRQ_raised = false;
+
+/* Local helpers */
+static uint8_t checksum8(const uint8_t *data, uint8_t len)
+{
+    uint16_t sum = 0;
+    for (uint8_t i = 0; i < len; i++)
+    {
+        sum += data[i];
+    }
+    return (uint8_t)(0x100 - (sum & 0xFF));
+}
+
+inline bool uid_equal(const uint8_t *a, uint8_t alen, const uint8_t *b, uint8_t blen)
+{
+    if (alen != blen) return false;
+    return (memcmp(a, b, alen) == 0);
+}
+
+static HAL_StatusTypeDef nfc_write_frame(NFC_handle *h, const uint8_t *data, uint8_t len)
+{
+    /* Frame: 00 00 FF LEN LCS [DATA...] DCS 00
+       Over I2C: leading 0x00 for "write data"
+    */
+    uint8_t buf[64];
+    if((uint16_t)len + 9u > sizeof(buf))
+    {
+        return HAL_ERROR;
+    }
+
+    uint8_t idx = 0;
+    buf[idx++] = 0x00; // I2C write
+    buf[idx++] = PN532_PREAMBLE;
+    buf[idx++] = PN532_STARTCODE1;
+    buf[idx++] = PN532_STARTCODE2;
+    buf[idx++] = len;
+    buf[idx++] = (uint8_t)(0x100 - len); // length checksum (LEN + LCS = 0x00 mod 256)
+    memcpy(&buf[idx], data, len);
+    idx += len;
+    buf[idx++] = checksum8(data, len); // data checksum
+    buf[idx++] = PN532_POSTAMBLE;
+
+    return HAL_I2C_Master_Transmit(h->hi2c, h->i2c_addr, buf, idx, 100);
+}
+
+static HAL_StatusTypeDef nfc_read_bytes(NFC_handle *h, uint8_t *out, uint16_t n)
+{
+    HAL_StatusTypeDef st = HAL_I2C_Master_Receive(h->hi2c, h->i2c_addr, out, n, 1000);
+    return st;
+}
+
+/* Wait until PN532 asserts IRQ */
+static HAL_StatusTypeDef nfc_wait_ready(NFC_handle *h, TickType_t timeout_ticks)
+{
+    uint32_t start = HAL_GetTick();
+    uint8_t status = 0;
+
+    do
+    {
+        if(IRQ_raised)
+        {
+            IRQ_raised = false;
+            return HAL_OK;
+        }
+    } while ((HAL_GetTick() - start) < timeout_ticks);
+
+    return HAL_TIMEOUT;
+}
+
+static HAL_StatusTypeDef nfc_read_ack(NFC_handle *h)
+{
+    uint8_t r[7] = {0};
+    HAL_StatusTypeDef st = nfc_read_bytes(h, r, sizeof(r));
+    if(st != HAL_OK)
+    {
+        return st;
+    }
+
+    /* Sometimes there is a leading status byte 0x01 */
+    if(r[0] == 0x01)
+    {
+        if(r[1]==0x00 && r[2]==0x00 && r[3]==0xFF && r[4]==0x00 && r[5]==0xFF && r[6]==0x00)
+        {
+            return HAL_OK;
+        }
+    }
+    else
+    {
+        if(r[0]==0x00 && r[1]==0x00 && r[2]==0xFF && r[3]==0x00 && r[4]==0xFF && r[5]==0x00)
+        {
+            return HAL_OK;
+        }
+    }
+    return HAL_ERROR;
+}
+
+static HAL_StatusTypeDef nfc_read_response(NFC_handle *h, uint8_t *out, uint8_t out_max, uint8_t *out_len)
+{
+    uint8_t r[64] = {0};
+    HAL_StatusTypeDef st = nfc_read_bytes(h, r, sizeof(r));
+    if(st != HAL_OK)
+    {
+        return st;
+    }
+
+    uint8_t i = 0;
+    if(r[0] == 0x01)
+    {
+         /* skip status */
+        i = 1;
+    }
+
+    if(r[i+0]!=0x00 || r[i+1]!=0x00 || r[i+2]!=0xFF)
+    {
+        return HAL_ERROR;
+    }
+
+    uint8_t len = r[i+3];
+    uint8_t lcs = r[i+4];
+    if((uint8_t)(len + lcs) != 0x00)
+    {
+        return HAL_ERROR;
+    }
+    if(len > out_max)
+    {
+        return HAL_ERROR;
+    }
+
+    memcpy(out, &r[i+5], len);
+    *out_len = len;
+    return HAL_OK;
+}
+
+static HAL_StatusTypeDef nfc_send_cmd_wait(
+    NFC_handle *h,
+    const uint8_t *cmd,
+    uint8_t cmd_len,
+    uint8_t *resp,
+    uint8_t resp_max,
+    uint8_t *resp_len,
+    TickType_t overall_timeout
+){
+    TickType_t start = xTaskGetTickCount();
+
+    // CMD
+    HAL_StatusTypeDef st = nfc_write_frame(h, cmd, cmd_len);
+    if(st != HAL_OK)
+    {
+        return st;
+    }
+
+    // ACK
+    st = nfc_wait_ready(h, PN532_ACK_TIMEOUT_TICKS);
+    if(st != HAL_OK)
+    {
+        return st;
+    }
+    st = nfc_read_ack(h);
+    if(st != HAL_OK)
+    {
+        return st;
+    }
+
+    // RESPONSE
+    TickType_t now = xTaskGetTickCount();
+    TickType_t remaining = (now - start < overall_timeout) ? (overall_timeout - (now - start)) : 0;
+    if(remaining == 0)
+    {
+        return HAL_TIMEOUT;
+    }
+    st = nfc_wait_ready(h, (remaining < PN532_RSP_TIMEOUT_TICKS) ? remaining : PN532_RSP_TIMEOUT_TICKS);
+    if(st != HAL_OK)
+    {
+        return st;
+    }
+    return nfc_read_response(h, resp, resp_max, resp_len);
+}
+
+/* Public API */
+void NFC_IrqFromIsr(NFC_handle *h)
+{
+    IRQ_raised = true;
+}
+
+HAL_StatusTypeDef NFC_Init(NFC_handle *h)
+{
+    cbuf_init(
+        &uids,
+        uidsArray,
+        sizeof(uidsArray) / sizeof(uidsArray[0]),
+        sizeof(NFC_uid)
+    );
+
+    if (h->rst_port) {
+        HAL_GPIO_WritePin(h->rst_port, h->rst_pin, GPIO_PIN_RESET);
+        HAL_Delay(10);
+        HAL_GPIO_WritePin(h->rst_port, h->rst_pin, GPIO_PIN_SET);
+        HAL_Delay(100);
+    }
+
+    /* SAMConfiguration */
+    uint8_t cmd[]  = { PN532_HOSTTOPN532, PN532_CMD_SAMCONFIGURATION, 0x01, 0x14, 0x01 };
+    uint8_t resp[32]; uint8_t resp_len = 0;
+
+    HAL_StatusTypeDef st = nfc_send_cmd_wait(h, cmd, sizeof(cmd), resp, sizeof(resp), &resp_len, pdMS_TO_TICKS(800));
+    if(st != HAL_OK)
+    {
+        return st;
+    }
+
+    /* Response should be: D5 15 ... */
+    if(resp_len < 2 || resp[0] != PN532_PN532TOHOST || resp[1] != (PN532_CMD_SAMCONFIGURATION + 1))
+    {
+        return HAL_ERROR;
+    }
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef NFC_ReadPassiveTargetID(
+    NFC_handle *h,
+    uint8_t *uid,
+    uint8_t *uid_len,
+    TickType_t timeout_ticks
+) {
+    if(!h || !uid || !uid_len)
+    {
+        return HAL_ERROR;
+    }
+
+    uint8_t cmd[]  = { PN532_HOSTTOPN532, PN532_CMD_INLISTPASSIVETARGET, 0x01, 0x00 };
+    uint8_t resp[32]; uint8_t resp_len = 0;
+
+    HAL_StatusTypeDef st = nfc_send_cmd_wait(h, cmd, sizeof(cmd), resp, sizeof(resp), &resp_len, timeout_ticks);
+    if(st != HAL_OK)
+    {
+        return st;
+    }
+
+    /* Response: D5 4B numTags ... */
+    if (resp_len < 3 || resp[0] != PN532_PN532TOHOST || resp[1] != (PN532_CMD_INLISTPASSIVETARGET + 1)) {
+        return HAL_ERROR;
+    }
+
+    uint8_t numTags = resp[2];
+    if(numTags < 1)
+    {
+        return HAL_ERROR;
+    }
+
+    /* Parse UID for ISO14443A:
+       [3]=Tg, [4..5]=SensRes, [6]=SelRes, [7]=NFCIDLen, [8..] UID bytes
+    */
+    if(resp_len < 9)
+    {
+        return HAL_ERROR;
+    }
+    uint8_t n = resp[7];
+    if(n == 0 || n > 10 || (uint8_t)(8 + n) > resp_len)
+    {
+        return HAL_ERROR;
+    }
+
+    memcpy(uid, &resp[8], n);
+    *uid_len = n;
+    return HAL_OK;
+}
